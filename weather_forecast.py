59aa5018cc18501d2fe371d082f80b214da81a37
Mookesh Dash <pkdash1947.er@gmail.com>
2021-10-19 23:59:27 +0530

    Updated code to fit in one line

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,3 +1,2 @@
 import requests
-res = requests.get('https://wttr.in/{}'.format(input('input the city name:-\n')))
-print(res.text)
+print(requests.get('https://wttr.in/{}'.format(input('input the city name:-\n'))).text)
